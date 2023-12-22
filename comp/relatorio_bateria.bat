f56b2b7f797e6c2547549804c4cb09132b890ebd
Douglas alves dos santos <dalves.info@gmail.com>
2023-12-22 20:48:17 -0300

    Remove intes antigos

--- comp/relatorio_bateria.bat
+++
@@ -1,9 +0,0 @@
-:start
-echo -----------------------
-echo 5: Relatorio de bateria
-echo -----------------------
-echo.
-powercfg /batteryreport
-move battery-report.html D:\scripts\Relatorios\
-echo.
-echo -- Teste concluido --
