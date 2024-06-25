e6534421930877d16049f07faebdd847afb45260
Douglas alves dos santos <dalves.info@gmail.com>
2024-06-24 21:36:51 -0300

    ajuste commit - arquivo

--- funcoes_tarefas/pacotes_detalhes.py
+++ funcoes_tarefas/pacotes_detalhes.py
@@ -1,58 +1,49 @@
 import os
-import sys
 import subprocess
 from termcolor import colored
 from variaveis.interface_config import *
 
 
 #--------------------------------------------
+
+myfile_pacote = r'C:/sistema_python/automacao_sh/altera_pacote.sh'
+
 # configuracoes do menu
 
-
-func_cabecalho('Lista os Pacotes do Python')
-print('\n')
-subprocess.run(["pip", "list",])
-os.system('pip list > C:/scripts_logs/info_pacotes/backupPIP_python.txt')
-subprocess.run(["ls", "-ltr", myfile_bkp_pip])
-print('\n')
-print('Backup efetuado para o arquivo.TXT')
-
-
-
+def opcoes_pacotes():
+    func_cabecalho('Lista os Pacotes do Python')
+    print('\n')
+    subprocess.run(["pip", "list",])
+    os.system('pip list > C:/scripts_logs/info_pacotes/backupPIP_python.txt')
+    sleep(15)
 
 func_cabecalho('Pacotes do Python')
 
 #------------------------------------------------
 # chamar a instalacao de pacotes
 
-myfile_pacote = r'C:/scripts/automacao_sh/altera_pacote.sh'
 
 aplicar = 0
 while aplicar != 3:
     print('''Funcoes: 
-    [1] Instalar
-    [2] Nao instalar
-    [3] Altera arquivos de BKP''')
+    [1] Validar pacotes
+    [2] Instalar ou atualizar
+    [3] Voltar ao menu anterior''')
     print('\n')
-    aplicar = str(input('Voce quer aplicar pacotes do Python? '))
+    aplicar = str(input('Escolha uma opcao: '))
     if aplicar == '1':
+        print('\n')
+        opcoes_pacotes()
+        os.startfile(myfile_pacote)
+        print('\n')
+    elif aplicar == '2':
         print('\n')
         exec(open("pacotes_config.bat").read())
         print('\n')
-    elif aplicar == '2':
-        func_cabecalho('Tudo bem volte quando quiser')
-        break
     else:
         aplicar == '3'
-        os.startfile(myfile_pacote)
-        print('\n')
-        print('Backup efetuado para o arquivo.TXT')
-        print('\n')
+        func_cabecalho('Tudo bem volte quando quiser')
+        break
+
 
 dados_pc()
-
-        #os.chdir('.\scripts\bkp_arquivos')
-        #subprocess.popen(["/scripts/bkp_arquivos", "ls", "-ltr"])
-
-#os.system('start C:\Windows\System32\cmd.exe /k %windir%\system32\ipconfig.exe')
-#os.system(' /scripts/funcoes/interface_rede.ps1')
